eb035a91c7940b8a18790f90254966a34abc4378
mewmew1999 <guerillaqq@gmail.com>
2021-01-15 15:38:44 +0800

    Creating bean and controller package

---
+++ src/java/bean/User.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author user
+ */
+public class User implements Serializable{
+
+    private String userName, userPassword, email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+     
+    public User() {
+    }
+    
+}
