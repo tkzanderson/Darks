14d30f15e220f4e52123112ec971c7c81e7ddfd2
Janice Balan <janice.balan18@gmail.com>
2021-01-15 17:09:37 +0800

    added Products model

---
+++ src/java/bean/Products.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author janic
+ */
+public class Products {
+    private String prodTitle, prodDescription, prodType;
+    private double prodPrice;
+    private int id;
+    private String prodImage;
+
+    public Products() {
+    }
+
+    public String getProdTitle() {
+        return prodTitle;
+    }
+
+    public void setProdTitle(String prodTitle) {
+        this.prodTitle = prodTitle;
+    }
+
+    public String getProdDescription() {
+        return prodDescription;
+    }
+
+    public void setProdDescription(String prodDescription) {
+        this.prodDescription = prodDescription;
+    }
+
+    public String getProdType() {
+        return prodType;
+    }
+
+    public void setProdType(String prodType) {
+        this.prodType = prodType;
+    }
+
+    public double getProdPrice() {
+        return prodPrice;
+    }
+
+    public void setProdPrice(double prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProdImage() {
+        return prodImage;
+    }
+
+    public void setProdImage(String prodImage) {
+        this.prodImage = prodImage;
+    }
+}
